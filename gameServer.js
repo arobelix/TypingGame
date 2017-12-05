e90ed4c94f9d0c02290aa63567601169e666b7aa
arobelix <amaskarenas@gmail.com>
2017-12-05 00:05:30 -0600

    added more functionality
    added final html page, need to add readme

--- gameServer.js
+++ gameServer.js
@@ -54,7 +54,7 @@ app.post('/start', function(req, res){
         }
     }
     if(currentUserFlag) {
-    parsedJSON.userNameData[req.body.user] = {"score": 0};
+    parsedJSON.userNameData[req.body.user] = {"score": 0, "timeStart": "default"};
     }
     fs.writeFileSync('userNameDataBase.json', JSON.stringify(parsedJSON, null, '\t'));
    
@@ -64,7 +64,19 @@ app.post('/start', function(req, res){
 ////////////////////////////////////////////////////////////
 ////////////////////Gameplay///////////////////////////////
 app.get('/gamePlay', function(req, res){    
-    //Record time, get userName put it into json//////////////////////////////////////////////
+
+    res.sendFile(__dirname + '/gamePlay.html');
+});
+app.post('/gamePlay', function(req, res){    
+    //Only for time recording//////////////////////////////////////////////
+    var rawJSON = fs.readFileSync("userNameDataBase.json");
+    var parsedJSON = JSON.parse(rawJSON);
+    var timeStart = new Date();
+    var time = timeStart.getHours() + " " + timeStart.getMinutes() + " " + timeStart.getSeconds();
+    console.log(req.body);    
+    parsedJSON.userNameData[req.body.user].timeStart = time;
+   
+    fs.writeFileSync('userNameDataBase.json', JSON.stringify(parsedJSON, null, '\t'));
     res.sendFile(__dirname + '/gamePlay.html');
 });
 app.get('/sentence.json', function(req, res){ 
@@ -80,8 +92,9 @@ app.get('/sentence.json', function(req, res){
 });
 app.post('/gameComplete', function(req, res){    
     console.log(req.body);
-    //Record time, get userName put it into json//////////////////////////////////////
-    //calculat score////////////////////////////////Also write gameComplete page where score is displayed to user.
-    res.sendFile(__dirname + '/gamePlay.html');
+    //Record time, get userName, calcualte score//////////////////////////////////////
+    //change score, then require page to request json file, move sentence to JSON////////////////////////////////Also write gameComplete page where score is displayed to user.
+    //to play new game make post request to start, also add link to go back to leaderboard
+    res.sendFile(__dirname + '/gameComplete.html');
 });
 ///////////////////////////////////////////////////////////////////
